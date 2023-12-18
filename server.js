f338cd4da54c98a3bdaddf053f9709cbcc012326
Niles Thompson <dewand.th@gmail.com>
2023-12-18 13:00:06 -0800

    added new database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(MONGODB_URL);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
